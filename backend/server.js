de01b183cad416faae8e5e32d131d716d3fd8a14
mohameddn988 <msideneche@gmail.com>
2024-06-22 23:05:59 +0100

    MongoDB Atlas & Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 //express app
@@ -17,7 +18,16 @@ app.use((req, res, next) =>{
 //routes
 app.use('/api/workouts', workoutRoutes)
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('Server is running on port', process.env.PORT)
-})
+//connect to db
+mongoose.connect(process.env.MONGO_UI)
+   .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to the db & Server is running on port', process.env.PORT)
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+
