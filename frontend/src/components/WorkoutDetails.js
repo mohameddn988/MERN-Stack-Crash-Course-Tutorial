00eac2a90c73c036776cc63484cd414529556f05
mohameddn988 <msideneche@gmail.com>
2024-07-28 21:28:47 +0100

    Deleting Data

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,10 +1,27 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
 const WorkoutDetails = ( {workout} ) => {
+    const { dispatch } = useWorkoutsContext()
+
+    const handleClick = async () => {
+        const response = await fetch('/api/workouts/' + workout._id, {
+                method: 'DELETE'
+            })  
+        const json = await response.json( )
+
+        if (response.ok) {
+            dispatch({ type: 'DELETE_WORKOUT', payload: json })
+            console.log("new workout added successfully", json)
+        }
+    }
+
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Load (kg):</strong> {workout.load} kg</p>
             <p><strong>Reps:</strong> {workout.reps} </p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>Delete</span>
         </div>
     )
 }
--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -2,19 +2,27 @@ import { createContext, useReducer } from "react"
 
 export const WorkoutContext = createContext()
 
-export const WorkoutsReducer = (state, action) => {
+export const workoutsReducer = (state, action) => {
     switch (action.type) {
         case 'SET_WORKOUTS':
-            return {workouts: action.payload }
+            return {
+                workouts: action.payload 
+            }
         case 'CREATE_WORKOUT':
-            return {workouts: [action.payload, ...state.workouts]}
+            return {
+                workouts: [action.payload, ...state.workouts]
+            }
+        case 'DELETE_WORKOUT':
+            return {
+                workouts: state.workouts.filter((w) => w._id !== action.payload._id)
+            }  
         default:
-            return state;
-    }
+            return state
+    }   
 }
 
 export const WorkoutContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(WorkoutsReducer, {
+    const [state, dispatch] = useReducer(workoutsReducer, {
         workouts: null
     })
 
